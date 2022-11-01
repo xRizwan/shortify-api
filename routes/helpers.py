4eca5c9d671870a73d55e5d634460629f37af20c
Muhammad Rizwan <xrizwanr@gmail.com>
2022-11-01 15:09:57 +0500

    added url validation

--- routes/helpers.py
+++
@@ -1,5 +0,0 @@
-def form_valid_url(long: str):
-    if "http" in long:
-        return long
-    else:
-        return f"https://{long}"
--- routes/urls.py
+++ routes/urls.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from dependencies import get_db, get_optional_current_user, get_current_user
 import db.schemas as schemas
 import db.crud as crud
-import routes.helpers as helpers
+from validators import url as url_validator
 
 router = APIRouter()
 
@@ -16,6 +16,8 @@ def get_urls(db: Session = Depends(get_db), user: schemas.User = Depends(get_opt
 
 @router.post("/api/shortify", response_model=schemas.URL)
 def shorten_url(url: schemas.URLCreate, db: Session = Depends(get_db), user: schemas.User | None = Depends(get_optional_current_user)):
+    if not url_validator(url.long):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid URL provided.")
     return crud.create_url(db=db, url=url, user=user)
 
 
@@ -23,8 +25,7 @@ def shorten_url(url: schemas.URLCreate, db: Session = Depends(get_db), user: sch
 def redirect(short_url, db: Session = Depends(get_db)):
     db_url = crud.get_url(db=db, short_url=short_url)
     if (db_url):
-        valid_url = helpers.form_valid_url(db_url.long)
-        return RedirectResponse(valid_url)
+        return RedirectResponse(db_url)
 
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
