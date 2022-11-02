eddcd725979838af79cfe298a3ff4fdb51d4b350
Muhammad Rizwan <xrizwanr@gmail.com>
2022-11-02 11:02:46 +0500

    Added CORS

--- main.py
+++ main.py
@@ -3,9 +3,20 @@ from db import models
 from db.database import engine
 from routes import users, urls
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = ["*"]
 
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users.router)
 app.include_router(urls.router)
