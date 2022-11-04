9241f2fded4e5065b94d2e2fb1e4cb09ae67e95f
Muhammad Rizwan <xrizwanr@gmail.com>
2022-11-04 14:54:10 +0500

    refactored tests and added test for user urls endpoint

--- tests/helpers.py
+++ tests/helpers.py
@@ -12,3 +12,24 @@ def login_user(client: TestClient, username, password) -> Response:
     return client.post(
         '/login', data={"username": username, "password": password}
     )
+
+
+def get_user_token(client, username, password) -> Response:
+    token = login_user(client, username, password).json()
+    return token['access_token']
+
+
+def get_auth_headers(client, username=None, password=None) -> dict:
+    headers = {}
+    if (username and password):
+        token = get_user_token(client, username, password)
+        headers = {"authorization": "Bearer " + token}
+    return headers
+
+
+def create_url(client: TestClient, url, headers={}) -> Response:
+    return client.post('/api/shortify', json={"long": url}, headers=headers)
+
+
+def get_user_urls(client: TestClient, headers={}) -> Response:
+    return client.get('/api/shortify', headers=headers)
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -2,12 +2,13 @@ from fastapi.testclient import TestClient
 from main import app
 from db_setup import test_db
 from utils import create_access_token
-from .helpers import create_user, login_user
+from .helpers import create_user, login_user, create_url, get_auth_headers, get_user_urls
 
 client = TestClient(app)
 
 test_username = "testeruser1"
 test_password = "testpassword123"
+
 
 def test_create_user(test_db):
     response = create_user(client, test_username, test_password)
@@ -28,14 +29,28 @@ def test_login(test_db):
 
 def test_shorten_url(test_db):
     url = "https://www.google.com"
-    response = client.post('/api/shortify', json={"long": url})
+    response = create_url(client, url)
 
     assert response.status_code == 200
     assert response.json()['long'] == url
 
+
 def test_invalid_url(test_db):
     url = "www.google.com"
-    response = client.post('/api/shortify', json={"long": url})
+    response = create_url(client, url)
 
     assert response.status_code == 400
     assert response.json()['detail'] == "Invalid URL provided."
+
+
+def test_user_urls(test_db):
+    url = "https://www.google.com"
+
+    create_user(client, test_username, test_password)
+    headers = get_auth_headers(client, test_username, test_password)
+
+    create_url(client, url, headers)
+    response = get_user_urls(client, headers)
+
+    assert response.status_code == 200
+    assert response.json()[0]['long'] == url
